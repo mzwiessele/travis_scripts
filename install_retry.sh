de314f767e95cc2a959edd01bccd1e1e487a2133
mzwiessele <ibinbei@gmail.com>
2016-02-29 14:02:24 +0000

    [install] retry

--- install_retry.sh
+++ install_retry.sh
@@ -2,7 +2,7 @@
 n=0
 until [ $n -ge 5 ]
 do
-	conda install --yes python=$PYTHON_VERSION numpy=1.9 scipy=0.16 nose pip six matplotlib sphinx && break
+	conda install --yes python=$PYTHON_VERSION numpy=1.9 scipy=0.17 nose pip six matplotlib sphinx && break
 	echo "Retry $n"
 	n=$[$n+1]
 done
