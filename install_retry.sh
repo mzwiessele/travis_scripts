1d15d8435c4f47cf5edb5415170ce77cfcd521aa
Max Zwiessele <ibinbei@gmail.com>
2018-02-22 17:42:05 +0100

    Update install_retry.sh

--- install_retry.sh
+++ install_retry.sh
@@ -2,7 +2,7 @@
 n=0
 until [ $n -ge 5 ]
 do
-	conda create -q -n build-environment python=$PYTHON_VERSION numpy scipy nose pip six matplotlib sphinx && activate build-environment && break
+	conda create --yes -q -n build-environment python=$PYTHON_VERSION numpy scipy nose pip six matplotlib sphinx && activate build-environment && break
 	echo "Retry $n"
 	n=$[$n+1]
 done
